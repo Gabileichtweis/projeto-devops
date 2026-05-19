45184700d1cdcc193bcae40301d643f40f977177
Gabriela Leichtweis <gabriela.leichtweis@sap.com>
2026-05-19 20:40:35 -0300

    add project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <main className="content">
+        <h1>Bem-vindo ao projeto de DevOps</h1>
+      </main>
+
+      <footer className="footer">
+        © 2026 Gabriela Leichtweis - Todos os direitos reservados
+      </footer>
     </div>
   );
 }
